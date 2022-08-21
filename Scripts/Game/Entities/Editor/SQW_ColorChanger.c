d7c4f6f7df260ec57adb000ffca15e0c12ed4c4e
Sqwince <sqwince@aol.com>
2022-08-21 11:16:48 -0500

    NoReplication
    cube is visible and triggerable from client and server, but not in sync. Server is not broadcasting color to clients. each generate their own random color locally

--- Scripts/Game/Entities/Editor/SQW_ColorChanger.c
+++ Scripts/Game/Entities/Editor/SQW_ColorChanger.c
@@ -16,21 +16,29 @@ class SQW_ColorChanger : ScriptComponent
 	All of these will change the material for a specific entity instance, so it's not global
 	*/
 	
+	protected BaseWorld m_World;
+	
+	//Cube
+	protected GameEntity m_Cube;
+	ParametricMaterialInstanceComponent m_ParametricMaterialinstanceComponent;
+	
+	
 	//location: X,Y,Z
 	vector m_vOrigin = {60, 1, 70};
 	
 	//Replication
-	[RplProp(onRplName: "OnColorChanged")]			// this value is to be updated by the authority, not set locally by proxies (even owner)
+	//[RplProp(onRplName: "OnColorChanged")]		// this value is to be updated by the authority, not set locally by proxies (even owner)
 	protected int m_Color; 							// if it is set locally, the change will not broadcast and there will be a difference between the proxy and the authority
 													// this state discrepancy will last until authority's next update broadcast
-	 
+	protected ref RandomGenerator m_pRandomGenerator;
+	
 	
 	// Attached component.
 	protected RplComponent m_pRplComponent;
-	protected GenericEntity m_pCube;
+
+	/*
 	protected ref Shape m_Sphere;
 	protected SQW_EditableCommentEntity m_pComment; 
-	protected ref RandomGenerator m_pRandomGenerator;
 	
 	//HUD
 	protected SCR_EditableCommentComponent m_NLoCTextBox;
@@ -38,43 +46,21 @@ class SQW_ColorChanger : ScriptComponent
 	protected SCR_EditableCommentComponent m_RplTextBox;
 	protected bool m_bReplicated;
 	protected SCR_EditableCommentComponent m_ColorTextBox;
-	
-	
+	*/
+
 	
 	//------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
 	protected void RpcAsk_Authority_Method(bool turningOn)
 	{
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/*	
 	//------------------------------------------------------------------------------------------------
 	// Called on the authority when an entity gets streamed
 	override bool RplSave(ScriptBitWriter writer)
-	{
-		
-		writer.Write(m_Color, 32);  //write 32 bits of m_Color - int is 32 bits in size
-			
+	{		
+		writer.Write(m_Color, 32);  //write 32 bits of m_Color - int is 32 bits in size		
 		return true;
 	}
 	
@@ -88,26 +74,22 @@ class SQW_ColorChanger : ScriptComponent
 			return false;
 		}
 		
-		m_Sphere = Shape.CreateSphere(m_Color, ShapeFlags.VISIBLE, m_vOrigin, 1);
-		
+				
 		return true;
 	}
-	
-	
-	
-	
+	*/
 	
 	
 	
 	
 	//------------------------------------------------------------------------------------------------
 	protected void OnTriggerActivate()
-	{			
-		int color = GetRandomColor();
-		m_Color = color;
-		
-		PrintFormat("New Color: %1", color.ToString());
-		m_Sphere.SetColor(color);		
+	{	
+				
+		//Change Color
+		m_Color = GetRandomColor();
+		m_ParametricMaterialinstanceComponent.SetColor(m_Color);
+			
 	}
 	
 	//------------------------------------------------------------------------------------------------
@@ -120,7 +102,7 @@ class SQW_ColorChanger : ScriptComponent
 		}
 		
 		//random 8bit color between 0x00000000 and 0xFFFFFFFF
-		 int _color = 0xff000000 | Math.RandomIntInclusive(0x0,0xffffff);
+		int _color = 0xff000000 | Math.RandomIntInclusive(0x0,0xffffff);
 	
 		return _color;
 	}
@@ -130,44 +112,60 @@ class SQW_ColorChanger : ScriptComponent
 	//------------------------------------------------------------------------------------------------
 	override void EOnInit(IEntity owner)
 	{
-			SCR_BaseTriggerEntity parentTrigger = SCR_BaseTriggerEntity.Cast(owner);
-			if (!parentTrigger)
-			{
-				Print("SQW_ColorChanger cannot find trigger, it is not a child of SCR_BaseTriggerEntity!");
-				return;
-			}
 
-			parentTrigger.GetOnActivate().Insert(OnTriggerActivate);	
 	}
 	
-	
-	//------------------------------------------------------------------------------------------------
-	protected bool VerifyComponentsPresent()
-	{
-		//Check for rplComponent
-		if (!m_pRplComponent)
-		{
-			Print("SQW_ColorChanger does not have a RplComponent attached!", LogLevel.ERROR);
-			return false;
-		}
 		
-
-		
-		Print("Found everything!",LogLevel.WARNING);
-		return true;
-	}
-	
-	
 	//------------------------------------------------------------------------------------------------
 	override void OnPostInit(IEntity owner)
 	{
+				
+		//world
+		m_World = GetGame().GetWorld();
+		if (!m_World)
+		{
+			Print("SQW_ColorChanger could not find World!", LogLevel.WARNING);
+			return;
+		}
+			
+		//Check for rplComponent
 		m_pRplComponent = RplComponent.Cast(owner.FindComponent(RplComponent));
-					
-		m_Sphere = Shape.CreateSphere(0xff000000, ShapeFlags.VISIBLE, m_vOrigin, 1);
-		
-		if (!VerifyComponentsPresent())
-		return;
+		if (!m_pRplComponent)
+		{
+			Print("SQW_ColorChanger does not have a RplComponent attached!", LogLevel.ERROR);
+			return;
+		}
+	
+		//attach to trigger
+		SCR_BaseTriggerEntity parentTrigger = SCR_BaseTriggerEntity.Cast(owner);
+		if (!parentTrigger)
+		{
+			Print("SQW_ColorChanger cannot find trigger, it is not a child of SCR_BaseTriggerEntity!");
+			return;
+		}
 
+		parentTrigger.GetOnActivate().Insert(OnTriggerActivate);
+		
+		//Cube
+		m_Cube = GameEntity.Cast(m_World.FindEntityByName("TheCube"));	
+			
+		if(!m_Cube)
+			{
+				Print("SQW_ColorChanger could not locate TheCube!", LogLevel.ERROR);
+				return;
+			}
+		
+		//cube ParametricMaterialInstanceComponent	
+		m_ParametricMaterialinstanceComponent = ParametricMaterialInstanceComponent.Cast(m_Cube.FindComponent(ParametricMaterialInstanceComponent));
+		
+		if(!m_ParametricMaterialinstanceComponent)
+			{
+				Print("Cube needs to have ParametricMaterialInstanceComponent included", LogLevel.ERROR);
+				return;
+			}			
+		
+		//Init completed
+		Print("Found everything!",LogLevel.WARNING);
 		SetEventMask(owner, EntityEvent.INIT);
 	}
 
